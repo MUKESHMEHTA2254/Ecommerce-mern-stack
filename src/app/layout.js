17e31d363f853d52cf7d2c15b57a3713cdb0c635
MUKESHMEHTA2254 <mehtasagar437@gmail.com>
2023-09-21 13:05:06 +0530

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+import GlobalState from '@/context'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalState>
+          <Navbar />
+          <main className='flex min-h-screen flex-col mt-[80px]'>{children}</main>
+          <Footer/>
+        </GlobalState>
+      </body>
     </html>
   )
 }
